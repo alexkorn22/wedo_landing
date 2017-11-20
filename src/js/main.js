3e6a02c57d63659c0fc77a0a5427261849361081
sergeydem <s.demjanenko@artorg.com.ua>
2017-11-20 15:07:03 +0200

    добавил фавикон перенес скрипт формы битрикса в main.js

--- src/js/main.js
+++ src/js/main.js
@@ -112,4 +112,17 @@ $( document ).ready(function() {
 */
 
 
+    //форма битрикса
+
+        (function(w,d,u,b){w['Bitrix24FormObject']=b;w[b] = w[b] || function(){arguments[0].ref=u;
+                (w[b].forms=w[b].forms||[]).push(arguments[0])};
+            if(w[b]['forms']) return;
+            s=d.createElement('script');r=1*new Date();s.async=1;s.src=u+'?'+r;
+            h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://wedo.bitrix24.ua/bitrix/js/crm/form_loader.js','b24form');
+
+    b24form({"id":"12","lang":"ru","sec":"g50il2","type":"button","click":""});
+
+
+
 });
