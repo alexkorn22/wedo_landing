edb04e8b43ba0ed475587464148c58b13a78961f
sergeydem <s.demjanenko@artorg.com.ua>
2018-05-07 16:54:19 +0300

    на моб в блоке результаты сделал слайдер

--- src/js/main.js
+++ src/js/main.js
@@ -65,6 +65,58 @@ $( document ).ready(function() {
         }
     });
 
+//слайдер графиков
+    $('.chart-slider1').owlCarousel({
+        loop:true, //Зацикливаем слайдер
+        margin:50, //Отступ от картино если выводите больше 1
+        autoplay:false, //Автозапуск слайдера
+        smartSpeed:2000, //Время движения слайда
+        autoplayTimeout:7000, //Время смены слайда
+        dots: false,
+        nav: true,
+        navText: ["<img src='../img/Arrows-Back-icon.png'>","<img src='../img/Arrows-Back-icon.png'>"],
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+            0:{
+                items:1,
+                dots: true,
+                nav: false
+            },
+            600:{
+                items:1,
+                dots: true,
+                nav: false
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    $('.chart-slider2').owlCarousel({
+        loop:true, //Зацикливаем слайдер
+        margin:50, //Отступ от картино если выводите больше 1
+        autoplay:false, //Автозапуск слайдера
+        smartSpeed:2000, //Время движения слайда
+        autoplayTimeout:7000, //Время смены слайда
+        dots: false,
+        nav: true,
+        navText: ["<img src='../img/Arrows-Back-icon.png'>","<img src='../img/Arrows-Back-icon.png'>"],
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+            0:{
+                items:1,
+                dots: true,
+                nav: false
+            },
+            600:{
+                items:1,
+                dots: true,
+                nav: false
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
 //селектрик. вроде кастомный
     $(function() {
         $('select').selectric();
